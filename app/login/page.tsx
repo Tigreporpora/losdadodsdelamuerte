e9cc5f63d420355fe61f94eb5b2d585df694b92f
DonAvier <michelelunghi98@gmail.com>
2024-10-15 00:34:05 +0200

    Stani

---
+++ app/login/page.tsx
@@ -0,0 +1,19 @@
+export default function Login() {
+    return (
+      <div>
+        
+        <div> 
+          <div>
+             <input type="text" placeholder="Username" />
+          </div>
+          <div>
+            <input type="password" placeholder="password" />
+          </div>
+          <div>
+             <button>Entra</button>
+          </div>
+        </div>
+  
+      </div> 
+    );
+  }
