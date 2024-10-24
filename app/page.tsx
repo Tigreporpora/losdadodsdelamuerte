59adaf07014ebda2d8ccae8e66671385380f5efc
Lopexissimo <101009904+Lopexissimo@users.noreply.github.com>
2024-10-24 19:08:55 +0200

    link added

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
       <h1>Los datos de la muerte</h1>
-      <div> 
+      <div>
         <div>
-           <button>Login</button>
+          <Link href="/login">
+            <button>Login</button>
+          </Link>
         </div>
         <div>
-           <button>Crea</button>
+          <Link href="/registrati">
+            <button>Registrati</button>
+          </Link>
         </div>
       </div>
 
-    </div> 
+    </div>
   );
 }
